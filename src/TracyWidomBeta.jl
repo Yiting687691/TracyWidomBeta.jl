68bf1749f7db57375d7c3ddea47827767fd9e113
Tom Trogdon <trogdon@uw.edu>
2022-02-11 16:06:08 -0800

    Some function wrapping

--- src/TracyWidomBeta.jl
+++ src/TracyWidomBeta.jl
@@ -1,20 +1,25 @@
 module TracyWidomBeta
+
 using LinearAlgebra,Statistics,SparseArrays,Random,Distributions,Plots,Printf,Trapz,RandomMatrices
-export F
-println("What is the value of beta?")
-num1=readline()
-beta=num = parse(Float64, num1)
-println("What is the value of s?")
-num2=readline()
-s=parse(Float64, num2)
-println("Do you want high accuracy or low accuracy?")
-answer=readline()
-if answer=="low"
-    include("finite.jl")
-elseif answer=="high"
-    include("BDF4.jl")
-else
-    println("Try to enter either low or high.")
-end
+
+export BDF4
+
+include("BDF4.jl")
+
+# println("What is the value of beta?")
+# num1=readline()
+# beta = parse(Float64, num1)
+# println("What is the value of s?")
+# num2=readline()
+# s=parse(Float64, num2)
+# println("Do you want high accuracy or low accuracy?")
+# answer=readline()
+# if answer=="low"
+#     include("finite.jl")
+# elseif answer=="high"
+#     BDF4(beta)
+# else
+#     println("Try to enter either low or high.")
+# end
 
 end
