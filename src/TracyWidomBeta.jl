65add8f8dbb24ca062f0c15ec38542b98c81a41c
Yiting Zhang <zjx687691@gmail.com>
2022-06-16 00:09:33 -0700

    some updates

--- src/TracyWidomBeta.jl
+++ src/TracyWidomBeta.jl
@@ -1,6 +1,6 @@
 module TracyWidomBeta
 
-using LinearAlgebra,Statistics,SparseArrays,Random,Distributions,Printf,Trapz,RandomMatrices,ApproxFun,SpecialFunctions,Plots
+using LinearAlgebra, Statistics, SparseArrays, Random, Distributions, Printf, Trapz, RandomMatrices, ApproxFun, SpecialFunctions, Plots
 
 include("one_step.jl")
 include("finite_cdf.jl")
