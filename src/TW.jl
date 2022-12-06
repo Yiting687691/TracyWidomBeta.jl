cb79643f633d01ef2eb501de4d94b32bea8e748f
Yiting Zhang <72955283+Yiting687691@users.noreply.github.com>
2022-12-05 20:10:35 -0800

    Update TW.jl

--- src/TW.jl
+++ src/TW.jl
@@ -1,10 +1,16 @@
-function TW(beta;method="finite")
-    if method=="finite"
-        sol=finite_cdf(beta)
+function TW(β;method="finite",type="cdf")
+    if method=="finite" && type=="cdf"
+        sol=finite_dis(β,1000)
         TW=sol[1]
-    elseif method=="BDF4"
-        sol=BDF4_cdf(beta)
+    elseif method=="finite" && type=="pdf"
+        sol=finite_dis(β,1000)
+        TW=sol[2]
+    elseif method=="spectral" && type=="cdf"
+        sol=spectral_dis(β,1000)
         TW=sol[1]
+    elseif method=="spectral" && type=="pdf"
+        sol=spectral_dis(β,1000)
+        TW=sol[2]
     else
         TW="Input valid method"
     end
