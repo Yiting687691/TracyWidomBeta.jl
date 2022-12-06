dfa6bbcbf4dc0ab275db6fc9051b75e9cc08c72b
Yiting Zhang <72955283+Yiting687691@users.noreply.github.com>
2022-12-05 22:39:01 -0800

    Update TW.jl

--- src/TW.jl
+++ src/TW.jl
@@ -1,10 +1,8 @@
-function TW(β,cheb_coef;method="finite")
+function TW(β,cheb_coef;method="finite",x0=13.0,xN=-10.0,Δx_f=-0.01,Δx_s=-0.002,M_f=10^3,M_s=8000,l=20)
     if method=="finite"
-        sol=finite_dis(β,cheb_coef)
-        TW=sol
+        TW=finite_dis(β,cheb_coef;x0,xN,Δx_f,M_f)
     elseif method=="spectral"
-        sol=spectral_dis(β,cheb_coef)
-        TW=sol
+        TW=spectral_dis(β,cheb_coef;x0,xN,Δx_s,M_s,l)
     else
         TW="Input valid method"
     end
