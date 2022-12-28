4c2489cf6bbed7998f14bef109c806ce73631db3
Yiting Zhang <72955283+Yiting687691@users.noreply.github.com>
2022-12-28 13:09:37 -0800

    Update TW.jl

--- src/TW.jl
+++ src/TW.jl
@@ -1,4 +1,4 @@
-function TW(β,cheb_coef;method="finite",x0=13.0,xN=-10.0,Δx_f=-0.01,Δx_s=-0.002,M_f=10^3,M_s=8000,l=20)
+function TW(β,cheb_coef;method="finite",x0=13.0,xN=-10.0,Δx_f=-0.01,Δx_s=-0.001,M_f=10^3,M_s=16000,l=20)
     if method=="finite"
         TW=finite_dis(β,cheb_coef;x0,xN,Δx_f,M_f)
     elseif method=="spectral"
