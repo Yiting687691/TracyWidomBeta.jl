c16e140ea358e65267c72e927b96e5ea4816daf5
Yiting Zhang <72955283+Yiting687691@users.noreply.github.com>
2023-02-21 17:45:25 -0800

    Update TW.jl

--- src/TW.jl
+++ src/TW.jl
@@ -1,4 +1,4 @@
-function TW(β;cheb=10^3,method="finite",interp=true,step="trapz",pdf=false,x0=13.0,xN=-10.0,Δx_f=-0.01,Δx_s=-0.001,M_f=Int(floor(-10/(Δx_f))),M_s=8000,l=10)
+function TW(β;cheb=10^3,method="finite",interp=true,step="trapz",pdf=false,x0=13.0,xN=-10.0,Δx_f=-0.001,Δx_s=-0.001,M_f=Int(floor(-1/(Δx_f))),M_s=8000,l=10)
     
     # Set up the time domain
     if method=="finite"
