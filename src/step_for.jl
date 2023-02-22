71060baf86f7a0fe0037f3d104a84cd7453799ed
Yiting Zhang <72955283+Yiting687691@users.noreply.github.com>
2023-02-21 17:46:59 -0800

    Update step_for.jl

--- src/step_for.jl
+++ src/step_for.jl
@@ -46,7 +46,7 @@ function step_for(method,step,A,B,x0,M_s,xs,xl,c0,c1,c2,c3,c4,c5,l)
                 TW_pdf[i+1]=c0_pdf[end]
             end
         end
-    elseif step=="bdf4" && method=="spectral"
+    elseif step=="bdf4"
         RR = (A+(x0*B))*(12Δx)
         IRR = 25I - RR
         ΔRR = (12Δx^2)*B
@@ -58,10 +58,15 @@ function step_for(method,step,A,B,x0,M_s,xs,xl,c0,c1,c2,c3,c4,c5,l)
             IRR -= ΔRR
             c0 = IRR\temp
             c0_pdf=(A+xs[i+1]*B)*c0
-            TW_cdf[i+1] = sum(c0.*integ)|> real
-            TW_pdf[i+1] = sum(c0_pdf.*integ)|> real
+            if method=="spectral"
+                TW_cdf[i+1] = sum(c0.*integ)|> real
+                TW_pdf[i+1] = sum(c0_pdf.*integ)|> real
+            elseif method=="finite"
+                TW_cdf[i+1]=c0[end]
+                TW_pdf[i+1]=c0_pdf[end]
+            end
         end
-    elseif step=="bdf5" && method=="spectral"
+    elseif step=="bdf5"
         RR = (A+(x0*B))*(60Δx)
         IRR = 137I - RR
         ΔRR = (60Δx^2)*B
@@ -74,10 +79,15 @@ function step_for(method,step,A,B,x0,M_s,xs,xl,c0,c1,c2,c3,c4,c5,l)
             IRR -= ΔRR
             c0 = IRR\temp
             c0_pdf=(A+xs[i+1]*B)*c0
-            TW_cdf[i+1] = sum(c0.*integ)|> real
-            TW_pdf[i+1] = sum(c0_pdf.*integ)|> real
+            if method=="spectral"
+                TW_cdf[i+1] = sum(c0.*integ)|> real
+                TW_pdf[i+1] = sum(c0_pdf.*integ)|> real
+            elseif method=="finite"
+                TW_cdf[i+1]=c0[end]
+                TW_pdf[i+1]=c0_pdf[end]
+            end
         end
-    elseif step=="bdf6" && method=="spectral"
+    elseif step=="bdf6"
         RR = (A+(x0*B))*(60Δx)
         IRR = 147I - RR
         ΔRR = (60Δx^2)*B
@@ -91,24 +101,14 @@ function step_for(method,step,A,B,x0,M_s,xs,xl,c0,c1,c2,c3,c4,c5,l)
             IRR -= ΔRR
             c0 = IRR\temp
             c0_pdf=(A+xs[i+1]*B)*c0
-            TW_cdf[i+1] = sum(c0.*integ)|> real
-            TW_pdf[i+1] = sum(c0_pdf.*integ)|> real
+            if method=="spectral"
+                TW_cdf[i+1] = sum(c0.*integ)|> real
+                TW_pdf[i+1] = sum(c0_pdf.*integ)|> real
+            elseif method=="finite"
+                TW_cdf[i+1]=c0[end]
+                TW_pdf[i+1]=c0_pdf[end]
+            end
         end
-    elseif step=="bdf4" || step=="bdf5" || step=="bdf6" && method=="finite"
-        RR = (A+(x0*B))*(6Δx)
-        IRR = 11I - RR
-        ΔRR = (6Δx^2)*B
-        for i = 1:xl-1
-            temp = 18c0 - 9c1 + 2c2
-            c2 = c1
-            c1 = c0
-            IRR -= ΔRR
-            c0 = IRR\temp
-            c0_pdf=(A+xs[i+1]*B)*c0
-            TW_cdf[i+1]=c0[end]
-            TW_pdf[i+1]=c0_pdf[end]
-        end
-        @warn "To ensure convergence, BDF3 is used instead!"
     end
     return TW_cdf,TW_pdf
 end
