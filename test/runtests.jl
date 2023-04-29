4ba88817e7a908455aab93effaf1e7adb2074cc2
Yiting Zhang <72955283+Yiting687691@users.noreply.github.com>
2023-04-29 14:00:54 -0700

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,15 @@
 using TracyWidomBeta
 using Test
 
-begin
-   1 == 1
+@testset "TW" begin
+    # Test typical inputs
+    F_f=TW(2)
+    F_s=TW(2;method="spectral",step="bdf5")
+    TWG=TracyWidom
+    @test F_f(-2) ≈ cdf(TWG,-2;beta=2,num_points=300) atol=1e-6
+    @test F_s(-2) ≈ cdf(TWG,-2;beta=2,num_points=300) atol=1e-11
+
+    # Test edge cases
+    @test F_f(-10) ≈ 0 atol=1e-6
+    @test F_s(-10) ≈ 0 atol=1e-11
 end
