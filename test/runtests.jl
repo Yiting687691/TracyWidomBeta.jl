96adb218b62afeb2a99a9dc3614d55576d9da8ec
Yiting Zhang <72955283+Yiting687691@users.noreply.github.com>
2023-04-29 20:17:23 -0700

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,12 +2,17 @@ using TracyWidomBeta
 using Test
 
 @testset "TracyWidomBeta.jl" begin
-    # Test typical inputs
+    # Test typical inputs around the peak (cdf and pdf)
     F_f=TW(2)
+    f_f=TW(2;pdf=true)
     F_s=TW(2;method="spectral",step="bdf5")
+    f_s=TW(2;method="spectral",step="bdf5",pdf=true)
     @test F_f(-2) ≈ F_s(-2) atol=1e-6
+    @test f_f(-2) ≈ f_s(-2) atol=1e-5
 
     # Test edge cases
-    @test F_f(-10) ≈ F_s(-10) atol=1e-6
-    @test F_f(13/sqrt(2)) ≈ F_s(13/sqrt(2)) atol=1e-6
+    @test F_f(-10) ≈ F_s(-10) atol=1e-12
+    @test F_f(13/sqrt(2)) ≈ F_s(13/sqrt(2)) atol=1e-12
+    @test f_f(-10) ≈ f_s(-10) atol=1e-11
+    @test f_f(13/sqrt(2)) ≈ f_s(13/sqrt(2)) atol=1e-11
 end
