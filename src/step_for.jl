0762b055f8b41e1a449ec3d322f7f133abac2763
Yiting Zhang <72955283+Yiting687691@users.noreply.github.com>
2023-10-01 12:40:13 -0700

    Update step_for.jl
    Add additional L-stable Runge-Kutta methods as alternative time-stepping options.

--- src/step_for.jl
+++ src/step_for.jl
@@ -1,4 +1,4 @@
-function step_for(method,step,A,B,x0,M_s,xs,xl,c0,c1,c2,c3,c4,c5,l)
+function step_for(method,step,A,B,x0,M_s,xs,xl,c0,c1,c2,c3,c4,c5,l;x33=0.4358665215,α=(2/sqrt(3))*cos(π/18),x34=1.06858)
     TW_cdf=zeros(xl,1)
     TW_pdf=zeros(xl,1)
     TW_cdf[1]=1
@@ -19,6 +19,126 @@ function step_for(method,step,A,B,x0,M_s,xs,xl,c0,c1,c2,c3,c4,c5,l)
             lhs=I-(Δx/2)*(A+xs[i+1]*B)
             c0=lhs\rhs
             c0_pdf=(A+xs[i+1]*B)*c0
+            if method=="spectral"
+                TW_cdf[i+1] = sum(c0.*integ)|> real
+                TW_pdf[i+1] = sum(c0_pdf.*integ)|> real
+            elseif method=="finite"
+                TW_cdf[i+1] = c0[end]
+                TW_pdf[i+1] = c0_pdf[end]
+            end
+        end
+    elseif step=="TRBDF2"    #TR-BDF2
+        for i=1:xl-1
+            rhs=(I+(Δx/4)*(A+xs[i]*B))*c0
+            lhs=I-(Δx/4)*(A+(xs[i]+(Δx/2))*B)
+            temp=lhs\rhs
+            rhs=(4/3)*temp-(1/3)*c0
+            lhs=I-(Δx/3)*(A+xs[i+1]*B)
+            c0=lhs\rhs
+            c0_pdf=(A+xs[i+1]*B)*c0
+            if method=="spectral"
+                TW_cdf[i+1] = sum(c0.*integ)|> real
+                TW_pdf[i+1] = sum(c0_pdf.*integ)|> real
+            elseif method=="finite"
+                TW_cdf[i+1] = c0[end]
+                TW_pdf[i+1] = c0_pdf[end]
+            end
+        end
+    elseif step=="irk23"   #Two-stage third-order
+        for i=1:xl-1
+            rhs1=c0
+            lhs1=I-Δx*(1/2+sqrt(3)/6)*(A+(xs[i]+Δx*(1/2+sqrt(3)/6))*B)
+            temp1=lhs1\rhs1
+            rhs2=c0-(sqrt(3)/3)*Δx*(A+(xs[i]+Δx*(1/2+sqrt(3)/6))*B)*temp1
+            lhs2=I-Δx*(1/2+sqrt(3)/6)*(A+(xs[i]+Δx*(1/2-sqrt(3)/6))*B)
+            temp2=lhs2\rhs2
+            c0=c0+(Δx/2)*((A+(xs[i]+Δx*(1/2+sqrt(3)/6))*B)*temp1+(A+(xs[i]+Δx*(1/2-sqrt(3)/6))*B)*temp2)
+            c0_pdf=(A+xs[i+1]*B)*c0
+            if method=="spectral"
+                TW_cdf[i+1] = sum(c0.*integ)|> real
+                TW_pdf[i+1] = sum(c0_pdf.*integ)|> real
+            elseif method=="finite"
+                TW_cdf[i+1] = c0[end]
+                TW_pdf[i+1] = c0_pdf[end]
+            end
+        end
+    elseif step=="irk33"    #Three-stage third-order
+        for i = 1:xl - 1
+            rhs1 = c0 
+            lhs1 = I-x33*Δx*(A+(xs[i]+x33*Δx)*B)
+            temp1 = lhs1\rhs1
+            rhs2 = c0+Δx*((1-x33)/2)*(A+(xs[i]+x33*Δx)*B)*temp1
+            lhs2 = I-x33*Δx*(A+(xs[i]+((1+x33)/2)*Δx)*B)
+            temp2 = lhs2\rhs2
+            rhs3 = c0+Δx*(-3*x33^2/2+4*x33-1/4)*(A+(xs[i]+x33*Δx)*B)*temp1+Δx*(3*x33^2/2-5*x33+5/4)*(A+(xs[i]+((1+x33)/2)*Δx)*B)*temp2
+            lhs3 = I-x33*Δx*(A+xs[i+1]*B)
+            c0 = lhs3\rhs3
+            c0_pdf=(A+xs[i+1]*B)*c0
+            if method=="spectral"
+                TW_cdf[i+1] = sum(c0.*integ)|> real
+                TW_pdf[i+1] = sum(c0_pdf.*integ)|> real
+            elseif method=="finite"
+                TW_cdf[i+1] = c0[end]
+                TW_pdf[i+1] = c0_pdf[end]
+            end
+        end
+    elseif step=="irk43"     #Four-stage third-order
+        for i=1:xl-1
+            rhs1=c0
+            lhs1=I-(Δx/2)*(A+(xs[i]+Δx/2)*B)
+            temp1=lhs1\rhs1
+            rhs2=c0+(Δx/6)*(A+(xs[i]+Δx/2)*B)*temp1
+            lhs2=I-(Δx/2)*(A+(xs[i]+2*Δx/3)*B)
+            temp2=lhs2\rhs2
+            rhs3=c0-(Δx/2)*(A+(xs[i]+Δx/2)*B)*temp1+(Δx/2)*(A+(xs[i]+2*Δx/3)*B)*temp2
+            lhs3=I-(Δx/2)*(A+(xs[i]+Δx/2)*B)
+            temp3=lhs3\rhs3
+            rhs4=c0+(3*Δx/2)*(A+(xs[i]+Δx/2)*B)*temp1-(3*Δx/2)*(A+(xs[i]+2*Δx/3)*B)*temp2+(Δx/2)*(A+(xs[i]+Δx/2)*B)*temp3
+            lhs4=I-(Δx/2)*(A+xs[i+1]*B)
+            c0=lhs4\rhs4
+            c0_pdf=(A+xs[i+1]*B)*c0
+            if method=="spectral"
+                TW_cdf[i+1] = sum(c0.*integ)|> real
+                TW_pdf[i+1] = sum(c0_pdf.*integ)|> real
+            elseif method=="finite"
+                TW_cdf[i+1] = c0[end]
+                TW_pdf[i+1] = c0_pdf[end]
+            end
+        end
+    elseif step=="irk34"       #Three-stage fourth-order
+        for i=1:xl-1
+            rhs1 = c0
+            lhs1 = I-Δx*((1+α)/2)*(A+(xs[i]+((1+α)/2)*Δx)*B)
+            temp1 = lhs1\rhs1
+            rhs2 = c0-(α/2)*Δx*(A+(xs[i]+Δx*((1+α)/2))*B)*temp1
+            lhs2 = I-Δx*((1+α)/2)*(A+(xs[i]+Δx/2)*B)
+            temp2 = lhs2\rhs2
+            rhs3 = c0+Δx*(1+α)*(A+(xs[i]+Δx*((1+α)/2))*B)*temp1-Δx*(1+2*α)*(A+(xs[i]+Δx/2)*B)*temp2
+            lhs3 = I-Δx*((1+α)/2)*(A+(xs[i]+((1-α)/2)*Δx)*B)
+            temp3 = lhs3\rhs3
+            c0 = c0+(Δx/(6*α^2))*(A+(xs[i]+((1+α)/2)*Δx)*B)*temp1+Δx*(1-(1/(3*α^2)))*(A+(xs[i]+Δx/2)*B)*temp2+(Δx/(6*α^2))*(A+(xs[i]+((1-α)/2)*Δx)*B)*temp3
+            c0_pdf=(A+xs[i+1]*B)*c0    
+            if method=="spectral"
+                TW_cdf[i+1] = sum(c0.*integ)|> real
+                TW_pdf[i+1] = sum(c0_pdf.*integ)|> real
+            elseif method=="finite"
+                TW_cdf[i+1] = c0[end]
+                TW_pdf[i+1] = c0_pdf[end]
+            end
+        end
+    elseif step=="irkn34"       #Three-stage fourth-order 
+        for i=1:xl-1 
+            rhs1 = c0
+            lhs1 = I-Δx*x34*(A+(xs[i]+x34*Δx)*B)
+            temp1 = lhs1\rhs1
+            rhs2 = c0+(1/2-x34)*Δx*(A+(xs[i]+Δx*x34)*B)*temp1
+            lhs2 = I-Δx*x34*(A+(xs[i]+Δx/2)*B)
+            temp2 = lhs2\rhs2
+            rhs3 = c0+Δx*2*x34*(A+(xs[i]+Δx*x34)*B)*temp1+Δx*(1-4*x34)*(A+(xs[i]+Δx/2)*B)*temp2
+            lhs3 = I-Δx*x34*(A+(xs[i]+(1-x34)*Δx)*B)
+            temp3 = lhs3\rhs3
+            c0 = c0+(Δx/(6*(1-2*x34)^2))*(A+(xs[i]+x34*Δx)*B)*temp1+Δx*(((3*(1-2*x34)^2)-1)/(3*(1-2*x34)^2))*(A+(xs[i]+Δx/2)*B)*temp2+(Δx/(6*(1-2*x34)^2))*(A+(xs[i]+(1-x34)*Δx)*B)*temp3
+            c0_pdf = (A+xs[i+1]*B)*c0
             if method=="spectral"
                 TW_cdf[i+1] = sum(c0.*integ)|> real
                 TW_pdf[i+1] = sum(c0_pdf.*integ)|> real
