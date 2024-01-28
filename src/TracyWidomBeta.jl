de7c32f7e6ea9c131a2ddd6c62c28cfad7b3f510
Yiting Zhang <72955283+Yiting687691@users.noreply.github.com>
2024-01-27 17:36:35 -0800

    Update TracyWidomBeta.jl

--- src/TracyWidomBeta.jl
+++ src/TracyWidomBeta.jl
@@ -2,7 +2,6 @@ module TracyWidomBeta
 
 using LinearAlgebra, Statistics, SparseArrays, Distributions, Trapz, ApproxFun, SpecialFunctions, FFTW
 
-include("time_gen.jl")
 include("initial_gen.jl")
 include("matrix_gen.jl")
 include("step_for.jl")
@@ -10,5 +9,13 @@ include("Fourier_interp.jl")
 include("TW.jl")
 
 export TW
+
+function time_gen(x1,x2,Δx)
+    xs=x1:Δx:x2 |> Array
+    if abs(xs[end])<abs(x2)
+        xs=x1:Δx:(x2+Δx) |> Array
+    end
+    return xs
+end
 
 end
